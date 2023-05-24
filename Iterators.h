b4d251ba7b04c8944ff3cfda1b14299a1f85394f
Franklyn Vasquez <franklynvasquez24@gmail.com>
2023-05-24 03:46:44 -0400

    Fixed access specifiers, added wrappers and iteration function pointer defs

--- Iterators.h
+++ Iterators.h
@@ -1,9 +1,15 @@
 #ifndef Iterators_h
 #define Iterators_h
 
-#define nullref(T) *(T*)nullptr
-#define static_Iterate(type, identifier) Iterate<type, static_array_length(type, identifier)>(identifier)
+#define nullref(T) (*(T*)nullptr)
+#define is_nullref(expr) (&expr == nullptr)
 
+#define ForEachIterator(type, name) void name(size_t index, type& value)
+#define ConstForEachIterator(type, name) void name(size_t index, const type& value)
+#define ForEachIteratorPointer(type, name) void(*name)(size_t, type&)
+#define ConstForEachIteratorPointer(type, name) void(*name)(size_t, const type&)
+
+#pragma region Iterators
 template <typename CollectionType, typename DereferenceType>
 class GeneralIterator
 {
@@ -73,7 +79,7 @@ struct Enumeration : DereferencedIteration
 };
 
 template <typename CollectionType, typename ValueType>
-class EnumerationIterator : GeneralIterator<CollectionType, Enumeration<ValueType>>
+class EnumerationIterator : public GeneralIterator<CollectionType, Enumeration<ValueType>>
 {
 public:
 	EnumerationIterator(CollectionType& collection, const size_t index)
@@ -91,7 +97,7 @@ template <typename Zipped1ValueType, typename Zipped2ValueType>
 struct Zipping : DereferencedIteration
 {
 	Zipping(Zipped1ValueType value1)
-		: DereferencedIteration(true), value1(value1), value2(Zipped2ValueType()), index(SIZE_MAX)
+		: DereferencedIteration(true), value1(value1), value2(nullref(Zipped2ValueType)), index(SIZE_MAX)
 	{
 	}
 
@@ -100,23 +106,13 @@ struct Zipping : DereferencedIteration
 	{
 	}
 
-	operator Zipped1ValueType& ()
-	{
-		return value1;
-	}
-
-	operator Zipped2ValueType& ()
-	{
-		return value2;
-	}
-
 	Zipped1ValueType value1;
 	Zipped2ValueType value2;
 	const size_t index;
 };
 
-template <typename CollectionType, typename Zipped1ValueType, typename Zipped2ValueType, typename Collection2Type = CollectionType>
-class ZipIterator : GeneralIterator<CollectionType, Zipping<Zipped1ValueType, Zipped2ValueType>>
+template <typename CollectionType, typename Zipped1ValueType, typename Zipped2ValueType = Zipped1ValueType, typename Collection2Type = CollectionType>
+class ZipIterator : public GeneralIterator<CollectionType, Zipping<Zipped1ValueType, Zipped2ValueType>>
 {
 public:
 	ZipIterator(CollectionType& collection, const size_t index)
@@ -140,7 +136,9 @@ public:
 protected:
 	Collection2Type& m_Collection2;
 };
+#pragma endregion
 
+#pragma region Iterables
 template <typename CollectionType, typename Iterator = GeneralIterator<CollectionType, CollectionType&>>
 class GeneralIterable
 {
@@ -187,11 +185,26 @@ public:
 protected:
 	Collection2Type& m_Collection2;
 };
+#pragma endregion
 
-template <typename CStyleArrayType, size_t Length>
-GeneralIterable<CStyleArrayType[Length], GeneralIterator<CStyleArrayType[Length], CStyleArrayType&>> Iterate(CStyleArrayType (&array)[Length])
+#pragma region Wrappers
+template <typename CStyleArrayType, size_t Length = sizeof(CStyleArrayType)>
+inline GeneralIterable<CStyleArrayType[Length], GeneralIterator<CStyleArrayType[Length], CStyleArrayType&>> Iterate(CStyleArrayType(&array)[Length])
 {
 	return GeneralIterable<CStyleArrayType[Length], GeneralIterator<CStyleArrayType[Length], CStyleArrayType&>>(array, 0, Length);
 }
+
+template <typename CStyleArrayType, size_t Length = sizeof(CStyleArrayType)>
+inline GeneralIterable<CStyleArrayType[Length], EnumerationIterator<CStyleArrayType[Length], CStyleArrayType&>> Enumerate(CStyleArrayType(&array)[Length])
+{
+	return GeneralIterable<CStyleArrayType[Length], EnumerationIterator<CStyleArrayType[Length], CStyleArrayType&>>(array, 0, Length);
+}
+
+template <typename CStyleArrayType1, typename CStyleArrayType2 = CStyleArrayType1, size_t Array1Length = sizeof(CStyleArrayType1), size_t Array2Length = sizeof(CStyleArrayType2)>
+inline ZipIterable<CStyleArrayType1[Array1Length], CStyleArrayType1, CStyleArrayType2, CStyleArrayType2[Array2Length]> Zip(CStyleArrayType1(&array1)[Array1Length], CStyleArrayType2(&array2)[Array2Length])
+{
+	return ZipIterable<CStyleArrayType1[Array1Length], CStyleArrayType1, CStyleArrayType2, CStyleArrayType2[Array2Length]>(array1, array2, 0, (Array1Length < Array2Length) ? Array1Length : Array2Length);
+}
+#pragma endregion
 
 #endif // !Iterators_h
