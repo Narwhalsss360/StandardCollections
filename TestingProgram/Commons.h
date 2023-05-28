26e4322ceca8fd23535dd167de8440ab86fb97b6
Franklyn Vasquez <franklynvasquez24@gmail.com>
2023-05-27 22:12:47 -0400

    added PrintCollection

--- TestingProgram/Commons.h
+++ TestingProgram/Commons.h
@@ -3,3 +3,38 @@
 #include "StandardCollections.h"
 
 constexpr int bomb_defusal_code_digits[] = { 7, 3, 5, 5, 6, 0, 8 };
+
+enum PrintStyles
+{
+	PythonStyle,
+	Indexed
+};
+
+template <typename CollectableType>
+void PrintCollection(Collection<CollectableType>& collection, PrintStyles printStyle = PrintStyles::PythonStyle)
+{
+	switch (printStyle)
+	{
+	case PythonStyle:
+		test_standard_output_write('[');
+		for (auto& i : Enumerate(collection))
+		{
+			test_standard_output_write(i.value);
+			if (i.index != collection.Length() - 1) test_standard_output_write(", ");
+		}
+		test_standard_output_write(']');
+		break;
+	case Indexed:
+		for (auto& i : Enumerate(collection))
+		{
+			test_standard_output_write('[');
+			test_standard_output_write(i.index);
+			test_standard_output_write("]:");
+			test_standard_output_write(i.value);
+			if (i.index != collection.Length() - 1) test_standard_output_write(", ");
+		}
+		break;
+	default:
+		break;
+	}
+}
