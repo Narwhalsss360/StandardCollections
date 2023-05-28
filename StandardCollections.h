25b749d5e2626506a6f5d83421db41696fbdb2e1
Franklyn Vasquez <franklynvasquez24@gmail.com>
2023-05-28 01:39:55 -0400

    Filter Restructure

--- StandardCollections.h
+++ StandardCollections.h
@@ -1,7 +1,7 @@
 #ifndef StandardCollections_h
 #define StandardCollections_h
 
-#pragma region Library Base
+#pragma region Source
 #include "CollectionDeclarations.h"
 #include "Iterators.h"
 #include "Collection.h"
@@ -9,7 +9,7 @@
 #include "IteratorWrappers.h"
 #pragma endregion
 
-#pragma region Custom Collections
+#pragma region Implemented Collections
 #include "Array.h"
 #include "DynamicArray.h"
 #pragma endregion
