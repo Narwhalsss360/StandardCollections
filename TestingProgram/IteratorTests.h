42312ccd53b4d6126ee4380c17ac3a83f52765c5
Franklyn Vasquez <franklynvasquez24@gmail.com>
2023-05-28 16:54:07 -0400

    Added test

--- TestingProgram/IteratorTests.h
+++ TestingProgram/IteratorTests.h
@@ -20,13 +20,28 @@ test_function(general_iteration)
 	auto begin_enumerator = EnumerationIterator<int[CollectionLength(array)], int&>(array, 0);
 	auto end_enumerator = EnumerationIterator<int[CollectionLength(array)], int&>(array, CollectionLength(array));
 
-	auto enumerable = GeneralIteratorWrapper<int[CollectionLength(array)], EnumerationIterator<int[CollectionLength(array)], int&>>(begin_enumerator, end_enumerator);
+	auto enumerable = GeneralIteratorWrapper<EnumerationIterator<int[CollectionLength(array)], int&>>(begin_enumerator, end_enumerator);
 
 	expectedIndex = 0;
 	for (auto i : Enumerate(begin_enumerator, end_enumerator))
 	{
 		test_expect(i.defaulted, false);
 		test_expect(i.value, array[expectedIndex]);
+		test_expect(i.index, expectedIndex);
+		expectedIndex++;
+	}
+
+	int array2[] = { 1, 2, 3, 4 };
+	
+	auto begin_zip = ZipIterator<int[CollectionLength(array)], int&>(array, array2, 0);
+	auto end_zip = ZipIterator<int[CollectionLength(array)], int&>(array, array2, CollectionLength(array));
+
+	expectedIndex = 0;
+	for (auto i : Zip(begin_zip, end_zip))
+	{
+		test_expect(i.defaulted, false);
+		test_expect(i.value1, array[expectedIndex]);
+		test_expect(i.value2, array2[expectedIndex]);
 		test_expect(i.index, expectedIndex);
 		expectedIndex++;
 	}
