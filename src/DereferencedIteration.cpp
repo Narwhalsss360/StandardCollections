6878eacb57fe32f9f365c8b767841b8be9b9ff64
Franklyn Vasquez <franklynvasquez24@gmail.com>
2023-06-01 22:31:33 -0400

    Removed unecessary include in translation unit

--- src/DereferencedIteration.cpp
+++ src/DereferencedIteration.cpp
@@ -1,4 +1,9 @@
-#include "CollectionDeclarations.h"
+struct DereferencedIteration
+{
+	DereferencedIteration(const bool defaulted = true);
+
+	const bool defaulted;
+};
 
 DereferencedIteration::DereferencedIteration(const bool defaulted)
 	: defaulted(defaulted)
